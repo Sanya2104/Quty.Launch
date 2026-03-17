b064db1a3afa0e2ce95046ea1c7a3f8a046f562e
Sanya2104 <60304509+Sanya2104@users.noreply.github.com>
2026-03-17 13:02:57 +0300

    Build 2603171302

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "by.quty.launch"
         minSdk = 29
         targetSdk = 36
-    versionCode = 14
+    versionCode = 15
     versionName = "0.0.9"
         versionNameSuffix = "Alpha"
     }
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
 
 }
+
 
 
 
