5f7a2d24d880da1fd5b878cb69222bba7b5d97ef
Sanya2104 <sa.kuzmin2010@yandex.by>
2026-07-10 17:52:07 +0300

    Build 260710_1751

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 29
         //noinspection OldTargetApi
         targetSdk = 36
-    versionCode = 125
-    versionName = "0.0.32"
+    versionCode = 129
+    versionName = "0.0.33"
         versionNameSuffix = "Alpha"
     }
 
@@ -59,6 +59,10 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
 
 }
+
+
+
+
 
 
 
